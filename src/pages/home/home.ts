f49a971bbc4387aa44b329c7390b36efadaaf4a1
Gary Ferdinand <gferdinand@foneclay.com>
2017-06-06 18:36:18 -0700

    Fixed cropping for 16:9 device with 4:3 camera.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,16 +10,18 @@ export class HomePage {
 
   cameraStatus: string;
   picture: string;
+  isTransparent: boolean;
 
   constructor(public navCtrl: NavController, private cameraPreview: CameraPreview) {
     this.picture = 'assets/icon/favicon.ico';
+    this.isTransparent = false;
   }
 
   cameraPreviewOpts: CameraPreviewOptions = {
     x: 0,
-    y: window.screen.height/4,
+    y: window.screen.height/2 - window.screen.width*4/6,
     width: window.screen.width,
-    height: window.screen.height/2,
+    height: window.screen.width*4/3,
     camera: 'front',
     tapPhoto: false,
     previewDrag: false,
@@ -28,16 +30,16 @@ export class HomePage {
   };
 
   pictureOpts: CameraPreviewPictureOptions = {
-    width: window.screen.width/2,
-    height: window.screen.height*0.15,
-    quality: 85
-  }
+
+  };
 
   startCamera() {
     this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
       (res) => {
+        this.isTransparent = true;
         console.log(res);
         this.cameraStatus = 'camera started';
+        console.log('screen is ' + window.screen.width + 'x' + window.screen.height);
       },
       (err) => {
         console.log(err);
@@ -58,28 +60,48 @@ export class HomePage {
     )
   }
 
-  /*getPictureSizes() {
+  getSizes() {
     this.cameraPreview.getSupportedPictureSizes().then(
-      (dimensions) => {
-        dimensions.forEach(
-          (dimension) => {
-            console.log(dimension.width + 'x' + dimension.height);
-          },
-          (err) => {
-            console.log('failed');
-          });
-      });
-  }*/
+      dimensions => {
+        dimensions.forEach(function(dimension) {
+          console.log(dimension.width + 'x' + dimension.height);
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  convert() {
+
+  }
 
   takePhoto() {
-    this.cameraPreview.takePicture(this.pictureOpts).then(
+    this.cameraPreview.takePicture().then(
       (imageData) => {
+        this.isTransparent = false;
         this.picture = 'data:image/jpeg;base64,' + imageData;
+        var canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        var image = new Image();
+        image.onload = function() {
+          console.log('image is ' + image.width + 'x' + image.height);
+          ctx.drawImage(image, 0, image.height*0.3, image.width, image.height*0.4, 0, 0, canvas.width, canvas.height);
+        };
+        image.src = 'data:image/png;base64,' + imageData;
       },
       (err) => {
         console.log(err);
         this.picture = 'assets/icon/favicon.ico';
       });
+  }
+
+  reset() {
+    this.isTransparent = true;
+    var canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
 }
